eed0ef04778d58eaa602354bf0aba1766907a936
Jurayev2509 <jurayevazizbek2525@gmail.com>
2023-01-29 22:39:51 +0500

    Type Convirsions | Operators | Comparision | Logical Operators

---
+++ lesson 3/index.js
@@ -0,0 +1,23 @@
+//// HOME WORK LESSON 2////
+
+////// let a = 1, b = 1;
+/////  let c = ++a; a bizda 1 ga teng   ++ bizga yana bir qiymat qoshib beradi  a 2 ga  boladi shunda bizda javob "c 2 " boladi
+/////  let d = b++;  b 1 ga teng, javob "d 1" boladi
+/////  console.log(c);
+/////  console.log(d);
+
+////////////ASSINMENT RESULT/////////////
+
+//let a = 2;
+//let x = 1 + (a *=2 )
+//console.log(x); javob 5 chiqadi chinku a ni qiymati 2 va 2 ni 2 ga kopaytirsak 4 boladi 4 ga x ni qiymatini qoshamz shunda javob x niki  5 boladi
+
+//////////TYPE CONVERSION ///////////////
+
+/// "" + 1 + 0
+// console.log("" - 1 + 0);
+/// "" - 1 + 0
+/// true + false  // consol da 1 chiqadi nima uchun false 0 ga teng true 1 ga teng shuning javob 1 bo'ladi
+// console.log(false + true );
+// console.log("4px" - 2);
+console.log(" -9 " - 5 + 2);
