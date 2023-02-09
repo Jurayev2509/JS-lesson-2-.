b2363546495e4a5c96d10f54b76747738383e8da
Jurayev2509 <jurayevazizbek2525@gmail.com>
2023-02-09 23:31:56 +0500

    Switch | For loop.

--- lesson 4/lesson-5/index.js
+++ lesson 4/lesson-5/index.js
@@ -15,7 +15,8 @@
 // break;
 
 // bunda ozgaruvchi bilan  case ichidagi nomi bir hil bolishi kerak agar biz malumotni ozgartirmoxchi bolsak // case 'bulut':// console.log("bugun bulut");// break; shu tarzda yozamiz
-
+// break bizga agar shart to'g'ri keladigan bo'lsa shu joyda to'xtatib beradi ;
+// agar break bo'lmasa bitta case to'g'ri kelsa kegingi xammasini chiqarib yuboradi ;
 // let i = 1
 // for (i; i<=10; i++){
 //  console.log(i);
@@ -29,6 +30,6 @@
 
 // switch :
 
-/// switch-xam if else , turnery bilan bitta vazifa bajaradi :
-//// farqi uni shartini aniq qilib beriladi ;
-//
+// switch-xam if else , turnery bilan bitta vazifa bajaradi :
+// farqi uni shartini aniq qilib beriladi ;
+
