70cbf80bda05b825fa470c26fc248b35fd681f8a
Jurayev2509 <jurayevazizbek2525@gmail.com>
2023-02-09 23:26:41 +0500

    Switch | For loop.

---
+++ lesson 4/lesson-5/index.js
@@ -0,0 +1,30 @@
+// Bugungi Mavzu  Switch | For loop.
+
+// let weather = 'yomgir'
+// weather = "bulut"
+
+//  switch(weather){
+//    case 'yomgir':
+//    case "bulut":
+// console.log('bugun yomgir');
+// break;
+// }
+
+// case 'bulut':
+// console.log("bugun bulut");
+// break;
+
+// bunda ozgaruvchi bilan  case ichidagi nomi bir hil bolishi kerak agar biz malumotni ozgartirmoxchi bolsak // case 'bulut':// console.log("bugun bulut");// break; shu tarzda yozamiz
+
+// let i = 1
+// for (i; i<=10; i++){
+//  console.log(i);
+// }
+
+
+// let i = 10;
+// for (i; i >= 1; i--) {
+//  console.log(i);
+// }
+
+//
